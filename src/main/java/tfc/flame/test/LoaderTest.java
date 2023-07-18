d218a96ca4b326d060b0aebf9ee163159f39a43d
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2023-07-18 12:13:13 -0400

    gradle build "support", granted this method isn't gonna be used at all

--- src/main/java/tfc/flame/test/LoaderTest.java
+++ src/main/java/tfc/flame/test/LoaderTest.java
@@ -7,31 +7,44 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 
 // so, if I put this in the test class path, then mixin refuses to not load
 public class LoaderTest {
     public static void main(String[] args) {
         try {
             ArrayList<URL> paths = new ArrayList<>();
+
             File fl = new File("out");
-            paths.add(new File(fl + "/test/classes").toURL());
-            paths.add(new File(fl + "/test/resources").toURL());
+            if (fl.exists()) {
+                paths.add(new File(fl + "/test/classes").toURL());
+                paths.add(new File(fl + "/test/resources").toURL());
+            } else {
+                fl = new File("build/classes");
+                if (fl.exists())
+                    for (File file : Objects.requireNonNull(fl.listFiles()))
+                        paths.add(new File(file + "/test").toURL());
+                fl = new File("build/resources/test");
+                if (fl.exists())
+                    paths.add(fl.toURL());
+            }
+
             paths.add(new URL(
                     "jar:file:/" + new File("mixin-0.8.5.jar").getAbsoluteFile() + "!/"
             ));
-            
+
             ClassLoader loader = (ClassLoader) JDKLoader.createLoader(paths.toArray(new URL[0]), FlameLoader.class.getClassLoader(), true);
-            
+
             Thread.currentThread().setContextClassLoader(loader);
-            
+
             {
                 Class<?> clazz = loader.loadClass("mixin.MixinLoader");
                 clazz.getMethod("init").invoke(null);
-                
+
                 clazz = loader.loadClass("mixin.test.MixinTarget");
                 clazz.getMethod("main", String[].class).invoke(null, (Object) args);
             }
-            
+
             {
                 Class<?> clazz = loader.loadClass("TestClass");
                 System.out.println(clazz.getCanonicalName());
