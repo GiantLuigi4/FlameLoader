0ed563fbc0c7120c2df8943345566f23991945be
LorenzoPapi <topolino123654@gmail.com>
2023-07-21 02:36:20 +0200

    target mixin tests

--- src/test/java/mixins/TargetMixin.java
+++ src/test/java/mixins/TargetMixin.java
@@ -8,8 +8,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = MixinTarget.class, remap = false)
 public class TargetMixin {
-	@Inject(at = @At("HEAD"), method = "main", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "main")
 	private static void preMain(String[] args, CallbackInfo ci) {
-		System.out.println("Hi");
+		System.out.println("Start of Main");
+	}
+	
+	// How do I inject it between the "i" variable declaration and the first system out println?
+	// @Inject(at = @At(opcode = ), method = "main")
+//	private static void beforePrint(String[] args, CallbackInfo ci) {
+//		System.out.println("PRINT");
+//	}
+	
+	@Inject(at = @At("RETURN"), method = "main")
+	private static void postMain(String[] args, CallbackInfo ci) {
+		System.out.println("End of Main");
 	}
 }
